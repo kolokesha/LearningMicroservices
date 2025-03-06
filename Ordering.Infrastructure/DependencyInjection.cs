452b9503e39b3534860c07acd9dd917ddb5741e5
alex <kolokesha@rambler.ru>
2025-03-06 16:24:06 +0300

    added dispatch interceptor

--- Ordering.Infrastructure/DependencyInjection.cs
+++ Ordering.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Infrastructure.Data;
@@ -11,10 +12,13 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrasturctureServices(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomianEventsInterceptor>();
         
-        services.AddDbContext<ApplicationDbContext>(options =>
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
         {
-            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
             options.UseSqlServer(connectionString);
         });
         return services;
