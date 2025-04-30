20cd28bca7db380756a66afacfce9bd2004063cc
Alexey.Kolomeitsev <Alexey.Kolomeitsev@henderson.ru>
2025-04-30 11:46:02 +0300

    added EventHandlers

--- Ordering.Application/DependencyInjection.cs
+++ Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using BuildingBlocks.Behaviours;
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Application.Data;
 using Ordering.Infrastructure.Data;
@@ -9,8 +10,12 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddMediatR(cfg =>
-            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
 
         services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
         
